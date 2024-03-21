55a7110c85dabf471a3906ef6fa39d9d5d5610e1
unknown <stifshin@willamette.edu>
2024-03-21 00:09:21 -0700

    fixedtestinglink

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,49 +2,61 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 import { TouchableOpacity } from 'react-native-web';
 import './styles.css'; // Import CSS file
+import Testing from './Testing';
+
 
 function HomePage() {
   return (
     <div className="app-container">
-    
-    <div>
-      {/* Top Bar */}
+      
       <div className="Top-bar">
-        {/* Add content for the top bar */}
-        <div className="Top-bar-content">
-          <p>This is the top bar content.</p>
+        <div className="top-bar-item-left">
+          <b>OREGON TURTLES</b>
+        </div>
+
+
+
+
+        <div className='top-bar-right-item'>
+          <Link to ="/testing">
+          <button><b> REGISTER</b>
+          </button>
+          </Link>
+        </div>
+        <div className='top-bar-right-item'>
+          <b>LOG IN</b>
+        </div>
+        <div className='top-bar-right-item'>
+          <b>MENU</b>
         </div>
       </div>
+
+      {/* <div className='centered-text'>
+        <div className='Header-text'> Oregon Turtles</div>
+        <div className='spacer'></div>
+        <p>Welcome! Oregon’s native freshwater turtles need your help! At this website you can learn more about turtles in our state, why our native species are in trouble, and what you can do to help. You can also download the Oregon Turtles App to help researchers keep track of turtles across the state by reporting your observations of turtles, or use the online reporting tool.</p>
+        <div className='spacer'></div>
+        <p>This website was developed by Western Oregon University, the Port of Portland, and the Oregon Native Turtle Working Group, a group formed to share expertise among various organizations and agencies involved in turtle conservation and to promote appreciation and conservation of turtles by all Oregonians. The Oregon Turtles App and web reporting tool was developed by Western Oregon University in collaboration with the Oregon Department of Fish & Wildlife and the City of Salem, with support from the Oregon Conservation and Recreation Fund.</p>
+      </div>
+
       <div className="spacer"></div>
-      <div className="spacer"></div>
+      <div className="spacer"></div> */}
+
 
       <Link to ="/testing">
-        <button> please god work</button>
-      </Link>
+    <button> please god work</button>
+  </Link>
 
-      
-      
-      {/* Main Content */}
-      <h1>Home Page</h1>
-      {/* Button to navigate to ProfilesPage */}
-      <Link to="/profiles">
-        <button>Go to Profiles Page</button>
-      </Link>
-      <div className="spacer"></div>
-      
-      {/* Button to navigate to NotFoundPage */}
-      <Link to="/not-found">
-        <button>Go to Not Found Page</button>
-      </Link>
+  
+
 
       {/* Bottom Bar */}
       <div className="bottom-bar">
         {/* Add content for the bottom bar */}
         <div className="bottom-bar-content">
-          <p>This is the bottom bar content.</p>
+          <div className='spacer'></div>
         </div>
       </div>
-    </div>
     </div>
   );
 }
--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 
+
 export default function NotFoundPage() {
   return (
     <div className="flex flex-col gap-2">
